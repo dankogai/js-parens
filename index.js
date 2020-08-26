9eb13444c333ffcf2b047b6fc667d9a4bf85b1cd
Dan Kogai <dankogai+github@gmail.com>
2020-08-26 17:09:42 +0900

    add combinators to exports

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // `export * as name` statement is still unavailable in many platforms
+import * as combinators from './combinators.js';
 import * as church from './church.js';
 import * as iota from './iota.js';
 import * as jot from './jot.js';
 import * as parens from './parens.js';
 import * as unlambda from './unlambda.js';
-export { church, iota, jot, parens, unlambda };
+export { combinators, church, iota, jot, parens, unlambda };
